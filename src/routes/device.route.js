7b594bfe2ca377f4a4cf9ea454db575999b69324
Habiboys <nouvalhabibie18@gmail.com>
2025-06-12 16:03:45 +0700

    bismillah

--- src/routes/device.route.js
+++ src/routes/device.route.js
@@ -12,11 +12,11 @@ const{
 const{ tokenValidation, verifyAdmin} = require('../middlewares/auth.middleware');
 
 // Basic CRUD routes
-router.post('/create', tokenValidation, verifyAdmin, createDevice);
+router.post('/create', tokenValidation, createDevice);
 router.get('/', tokenValidation, getAllDevices);
 router.get('/:id', tokenValidation, getDeviceById);
-router.put('/update/:id', tokenValidation, verifyAdmin, updateDevice);
-router.delete('/delete/:id', tokenValidation, verifyAdmin, deleteDevice);
+router.put('/update/:id', tokenValidation,  updateDevice);
+router.delete('/delete/:id', tokenValidation, deleteDevice);
 
 // WebSocket device routes
 router.get('/connected', tokenValidation, getAllConnectedDevices);
