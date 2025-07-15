9479bedf6c08fe414da2d02669b38495eaa864dc
Habiboys <nouvalhabibie18@gmail.com>
2025-07-15 10:43:53 +0700

    resolve error2

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {tokenValidation, verifyAdmin} = require('../middlewares/auth.middleware')
-const{login, refreshToken, createUser} = require('../controllers/auth.controller')
+
+const{login, refreshToken} = require('../controllers/auth.controller')
 
 router.post('/login', login)
 router.post('/refresh-token', refreshToken)
