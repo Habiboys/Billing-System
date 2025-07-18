98cb8fba53c862fc5ac1fd5f0e55105e9c5ca996
Habiboys <nouvalhabibie18@gmail.com>
2025-07-18 18:27:00 +0700

    new api

--- src/config/config.js
+++ src/config/config.js
@@ -7,7 +7,8 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    timezone: '+07:00'
   },
   test: {
     username: process.env.DB_USERNAME || 'root',
